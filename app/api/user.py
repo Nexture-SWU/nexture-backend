f7c88fc7fb9fadab84b60ffb402cdce9fe6b1c78
I-Chanu <chanuel0110@gmail.com>
2025-11-14 14:20:58 +0900

    Chore 공백 수정

--- app/api/user.py
+++ app/api/user.py
@@ -34,15 +34,3 @@ def get_my_profile(uuid: str = Depends(auth.get_current_user)):
         "id": user_data["id"],
         "name": user_data["name"] if user_data["name"] else "",
     }
-
-# # 회원 탈퇴
-# @router.delete("/api/me")
-# def quit(req: QuitRequest, user_id: str = Depends(auth.get_current_user)):
-#     user_data = user_service.get_user(user_id)
-#     if not user_data:
-#         raise HTTPException(status_code=400, detail="사용자를 찾을 수 없습니다.")
-#     if not user_service.verify_password(req.password, user_data["password"]):
-#         raise HTTPException(status_code=400, detail="비밀번호가 일치하지 않습니다.")
-#     if not user_service.delete_user(user_id):
-#         raise HTTPException(status_code=400, detail="회원 탈퇴에 실패했습니다.")
-#     return {"message": "회원 탈퇴가 완료되었습니다."}
